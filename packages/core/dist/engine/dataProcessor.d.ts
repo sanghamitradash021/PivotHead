e14c40c70117fcfae41e543826580c43b7f2c32f
mfsi.sudhanshu <mfsi.sudhanshuk>
2024-12-06 16:55:57 +0530

    bug:[12] Remove unnecessary folders

--- packages/core/dist/engine/dataProcessor.d.ts
+++
@@ -1,3 +0,0 @@
-import { PivotTableConfig, SortConfig } from '../types/interfaces';
-export declare function processData<T extends Record<string, any>>(config: PivotTableConfig<T>, sortConfig?: SortConfig | null): T[];
-//# sourceMappingURL=dataProcessor.d.ts.map
