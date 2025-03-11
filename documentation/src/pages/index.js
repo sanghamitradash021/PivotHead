04b477a9cda561b12a08f1b11f39d8dcefdaf473
anandmindfire <anand.k@mindfiresolutions.com>
2025-03-11 16:43:53 +0530

    fix(core): fix the meta data and add animation in docs

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -4,16 +4,31 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            style={{
+              backgroundColor: '#e62e2d',
+              color: 'white',
+              border: '2px solid #e62e2d',
+            }}
+            to="/docs/what-is-pivothead"
+          >
+            Get Started - 5min ⏱️
+          </Link>
+        </div>
       </div>
     </header>
   );
@@ -24,7 +39,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="PivotHead is a powerful tool for creating pivot tables in JavaScript. It is a lightweight library that is easy to use and integrate into your project."
     >
       <HomepageHeader />
       <main>
