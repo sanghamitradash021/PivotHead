c511bd5d9aee60b2f7c51a9ffedcfa74b4d02229
abhi_sek12 <abhisek.m@mindfiresolutions.com>
2025-03-26 20:59:04 +0530

    feat(core): add unit test for export services to pivot engine

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
+    environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
   resolve: {
