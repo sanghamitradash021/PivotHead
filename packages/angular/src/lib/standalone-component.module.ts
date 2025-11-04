5e85c4d4addb4f0e200d3947b5446f506649fa6c
sanghamitra dash <Sanghamitrad@mindfiresolutions.com>
2025-11-04 16:06:30 +0530

    fix(angular): files modified for the wrapper

--- packages/angular/src/lib/standalone-component.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { PivotHeadWrapperComponent } from './pivothead-wrapper.component';
-
-@NgModule({
-  imports: [PivotHeadWrapperComponent],
-  exports: [PivotHeadWrapperComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-})
-export class StandaloneComponentModule {}
