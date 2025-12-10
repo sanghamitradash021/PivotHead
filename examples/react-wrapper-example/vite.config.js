7b3b9bde0ef704dc65f2ce5574ec80456930d67b
sanghamitra dash <Sanghamitrad@mindfiresolutions.com>
2025-12-10 20:52:00 +0530

    fix(core): modified the vite config file

--- examples/react-wrapper-example/vite.config.js
+++ examples/react-wrapper-example/vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mindfiredigital/pivothead': path.resolve(
+        __dirname,
+        '../../packages/core/dist/pivothead-core.mjs'
+      ),
+      '@mindfiredigital/pivothead-react': path.resolve(
+        __dirname,
+        '../../packages/react/dist/index.js'
+      ),
+      '@mindfiredigital/pivothead-web-component': path.resolve(
+        __dirname,
+        '../../packages/web-component/dist/pivot-head.mjs'
+      ),
+    },
+  },
 });
