ad090f5594e461aa488e78eae8872091cb83c7ab
sanghamitra dash <Sanghamitrad@mindfiresolutions.com>
2026-01-08 11:48:35 +0530

    fix(angular): modified the angular.json file

--- examples/angular-demo/src/main.ts
+++ examples/angular-demo/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+// Import web component to register custom element
+import '@mindfiredigital/pivothead-web-component';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)],
